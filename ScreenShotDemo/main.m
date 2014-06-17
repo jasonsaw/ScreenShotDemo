6758a0dc9b642819591a216b622f7f1d13d3329a
mysoft <yaowu01@Luoyan-Mac-Lion.local>
2014-06-17 10:25:50 +0800

    Initial Commit

---
+++ ScreenShotDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScreenShotDemo
+//
+//  Created by mysoft on 6/17/14.
+//  Copyright (c) 2014 mysoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MYAppDelegate class]));
+    }
+}
